91330105c32ccf5bfd34a5c41034668d5fc2fc3c
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-16 19:56:09 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  texas_schools = ["UTA", "UT", "A&M", "TTech"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [], array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  texas_schools.unshift("SMU")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  texas_schools.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  texas_schools.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  texas_schools[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  texas_schools[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  texas_schools[-1]
 end
