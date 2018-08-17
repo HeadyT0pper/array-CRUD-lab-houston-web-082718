08f1318f518e61eb6531be25a45e7e3e979dbafc
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-17 00:27:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   texas_schools = ["UTA", "UT", "A&M", "TTech"]
 end
 
-def add_element_to_end_of_array(array, element)
-  ("array = []", "array.push("hi")")
+def add_element_to_end_of_array(array = [], element = [1])
+  add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
